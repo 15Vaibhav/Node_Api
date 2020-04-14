835787583a8418f8984371a509cb4ba75fbb1487
vaibhav <vp612512@gmail.com>
2020-04-14 19:28:38 +0530

    task api

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable func-names */
+/* eslint-disable no-use-before-define */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -13,12 +18,71 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     validate(value) {
         if (!validator.isEmail(value)) {
         throw new Error('email is not valid');
         }
     },
   },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
 });
 
-module.exports = mongoose.model('User', userSchema);
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateToken = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id.toString() }, 'thisisserver');
+  return token;
+};
+
+/**
+ * Login check email and password
+ */
+userSchema.statics.findByEmailAndPassword = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('User email is not found');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('password does not mathced');
+  }
+  return user;
+};
+
+/**
+ * Hashing password on create or update
+ */
+// eslint-disable-next-line func-names
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,18 +1,18 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-return-assign */
 const express = require('express');
 const User = require('../models/user');
+const Task = require('../models/task');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+router.get('/user/me', auth, async (req, res) => {
+        res.send(req.user);
 });
 
-router.get('/user/:id', async (req, res) => {
+router.get('/user/:id', auth, async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
 
@@ -29,24 +29,105 @@ router.get('/user/:id', async (req, res) => {
 router.post('/createUser', async (request, response) => {
     const user = new User(request.body);
     try {
+        const token = await user.generateToken();
+        user.tokens = user.tokens.concat({ token });
         await user.save();
-        response.status(201).send(user);
+        response.status(201).send({ user, token });
     } catch (error) {
         response.status(400).send(error);
     }
 });
 
-router.patch('/updateName/:id', async (req, res) => {
+router.post('/login', async (request, response) => {
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body,
-            { new: true, runValidators: true });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
+       const user = await User.findByEmailAndPassword(request.body.email, request.body.password);
+       const token = await user.generateToken();
+       user.tokens = user.tokens.concat({ token });
+       await user.save();
+       response.send({ user, token });
+    } catch (error) {
+        console.log(error);
+        response.status(400).send(error);
+    }
+});
+
+router.post('/logout', auth, async (request, response) => {
+    try {
+        request.user.tokens = request.user.tokens.filter((token) => token.token !== request.token);
+        await request.user.save();
+        response.send();
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.post('/logoutAll', auth, async (request, response) => {
+    try {
+        request.user.tokens = [];
+        await request.user.save();
+        response.send();
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.patch('/update', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    try {
+        updates.forEach((key) => req.user[key] = req.body[key]);
+        await req.user.save();
+        res.send(req.user);
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
+router.delete('/delete', auth, async (request, response) => {
+try {
+    await request.user.remove();
+    response.send(request.user);
+} catch (error) {
+    response.status(500).send();
+}
+});
+
+router.post('/createTask', auth, async (req, res) => {
+    console.log('task');
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id,
+    });
+    try {
+        await task.save();
+        res.status(201).send({ task });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.get('/getTaskById/:id', auth, async (req, res) => {
+    const _id = req.params.id;
+    console.log(_id);
+    console.log(req.user._id);
+    try {
+        const task = await Task.findOne({ _id, owner: req.user._id });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+    }
+});
+
+router.get('/tasks', auth, async (req, res) => {
+    try {
+        const tasks = await Task.find({ owner: req.user.id });
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
 module.exports = router;
